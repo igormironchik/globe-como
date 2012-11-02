1b640b2bbe87675dd821a2bdbf37d26d19a394e3
igor.mironchik@gmail.com <igor.mironchik@gmail.com@623d316c-ae11-33bf-eefa-44d4d3d9b093>
2012-11-02 08:36:31 +0000

    Work on channels.

---
+++ trunk/Globe/channel_timeout_widget.hpp
@@ -0,0 +1,85 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2012 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef GLOBE__CHANNEL_TIMEOUT_WIDGET_HPP__INCLUDED
+#define GLOBE__CHANNEL_TIMEOUT_WIDGET_HPP__INCLUDED
+
+// Qt include.
+#include <QtGui/QWidget>
+#include <QtCore/QScopedPointer>
+
+
+namespace Globe {
+
+class ChannelTimeoutWidgetPrivate;
+
+//
+// ChannelTimeoutWidget
+//
+
+/*!
+	Widget that displays current timeout in the channel
+	and allow to set timeout in the channel.
+*/
+class ChannelTimeoutWidget
+	:	public QWidget
+{
+	Q_OBJECT
+
+signals:
+	//! Emits when user change timeout.
+	void timeoutChanged( int msecs );
+
+public:
+	ChannelTimeoutWidget( int timeout = 0,
+		QWidget * parent = 0, Qt::WindowFlags f = 0 );
+
+	QSize sizeHint() const;
+	QSize minimumSizeHint() const;
+
+protected:
+	void paintEvent( QPaintEvent * event );
+	void mouseMoveEvent( QMouseEvent * event );
+	void mousePressEvent( QMouseEvent * event );
+	void mouseReleaseEvent( QMouseEvent * event );
+	bool event( QEvent * event );
+	void toolTipEvent( QEvent * event );
+
+private:
+	//! \return Tool tip for current state of the widget.
+	QString prepareToolTip() const;
+
+private:
+	QScopedPointer< ChannelTimeoutWidgetPrivate > d;
+}; // class ChannelTimeoutWidget
+
+} /* namespace Globe */
+
+#endif // GLOBE__CHANNEL_TIMEOUT_WIDGET_HPP__INCLUDED
---
+++ trunk/Globe/mainwindow.hpp
@@ -0,0 +1,72 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2012 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef GLOBE__MAINWINDOW_HPP__INCLUDED
+#define GLOBE__MAINWINDOW_HPP__INCLUDED
+
+// Qt include.
+#include <QtGui/QMainWindow>
+#include <QtCore/QScopedPointer>
+
+
+namespace Globe {
+
+class ChannelsManager;
+class DB;
+
+class MainWindowPrivate;
+
+//
+// MainWindow
+//
+
+//! Main window of the application.
+class MainWindow
+	:	public QMainWindow
+{
+public:
+	MainWindow( ChannelsManager * channelsManager, DB * db,
+		QWidget * parent = 0, Qt::WindowFlags flags = 0 );
+
+	~MainWindow();
+
+private:
+	//! Init.
+	void init();
+
+private:
+	Q_DISABLE_COPY( MainWindow )
+
+	QScopedPointer< MainWindowPrivate > d;
+}; // class MainWindow
+
+} /* namespace Globe */
+
+#endif // GLOBE__MAINWINDOW_HPP__INCLUDED

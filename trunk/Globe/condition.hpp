21c6ebb63069c1c5289e5ae7d16c10073f640bd6
igor.mironchik@gmail.com <igor.mironchik@gmail.com@623d316c-ae11-33bf-eefa-44d4d3d9b093>
2012-11-28 15:02:31 +0000

    Work on source's properties has been begun.

---
+++ trunk/Globe/condition.hpp
@@ -0,0 +1,125 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2012 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef GLOBE__CONDITION_HPP__INCLUDED
+#define GLOBE__CONDITION_HPP__INCLUDED
+
+// Qt include.
+#include <QtCore/QVariant>
+#include <QtCore/QString>
+
+// Como include.
+#include <Como/Source>
+
+
+namespace Globe {
+
+//! Expression in the condition
+enum Expression {
+	//! If less or equal.
+	IfLessOrEqual,
+	//! If less.
+	IfLess,
+	//! If equal.
+	IfEqual,
+	//! If greater.
+	IfGreater,
+	//! If greater or equal.
+	IfGreaterOrEqual
+}; // enum Expression
+
+
+//! Level of severity of the condition.
+enum Level {
+	//! Critical state of the source.
+	Critical = 0x01,
+	//! Error state of the source.
+	Error = 0x02,
+	//! Warning state of the source.
+	Warning = 0x03,
+	//! Debug state of the source.
+	Debug = 0x04,
+	//! Info state of the source (all is ok).
+	Info = 0x05
+}; // enum Level
+
+
+//
+// Condition
+//
+
+//! Condition in the properties of the Como source.
+class Condition {
+public:
+	explicit Condition( Como::Source::Type t );
+
+	Condition( const Condition & other );
+
+	Condition & operator = ( const Condition & other );
+
+	//! Check if this condition is match the given value.
+	bool check( const QVariant & val );
+
+	//! \return Tpe of the condition (Expression).
+	Expression type() const;
+	//! Set type of the condition (Expression).
+	void setType( Expression expr );
+
+	//! \return Value for the comparison.
+	const QVariant & value() const;
+	//! Set value fot the comparison.
+	void setValue( const QVariant & v );
+
+	//! \return Level of severity of the source.
+	Level level() const;
+	//! Set level of severity of the source.
+	void setLevel( Level l );
+
+	//! \return Message for this condition.
+	const QString & message() const;
+	//! Set message for this condition.
+	void setMessage( const QString & msg );
+
+private:
+	//! Type of the value.
+	Como::Source::Type m_valueType;
+	//! Expression type.
+	Expression m_exprType;
+	//! Value for the comparison.
+	QVariant m_value;
+	//! Level of severity.
+	Level m_level;
+	//! Message.
+	QString m_message;
+}; // class Condition
+
+} /* namespace Globe */
+
+#endif // GLOBE__CONDITION_HPP__INCLUDED
---
+++ trunk/Globe/properties.hpp
@@ -0,0 +1,85 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2012 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef GLOBE__PROPERTIES_HPP__INCLUDED
+#define GLOBE__PROPERTIES_HPP__INCLUDED
+
+// Como include.
+#include <Como/Source>
+
+// Qt include.
+#include <QtCore/QList>
+
+// Globe include.
+#include <Globe/condition.hpp>
+
+
+namespace Globe {
+
+//
+// Properties
+//
+
+//! Properties of the source.
+class Properties {
+public:
+	explicit Properties( const Como::Source & source );
+
+	Properties( const Properties & other );
+
+	Properties & operator = ( const Properties & other );
+
+	//! \return Priority of the source.
+	int priority() const;
+	//! Set priority of the source.
+	void setPriority( int p );
+
+	//! \return Amount of conditions.
+	int conditionsAmount() const;
+	//! \return Condition with the given index.
+	Condition & conditionAt( int index );
+	//! Insert new condition to the new place with \arg index.
+	//! If \arg index is -1 then condition will be placed to the end of list.
+	void insertCondition( const Condition & cond, int index = -1 );
+	//! Remove condition with the \arg index.
+	void removeCondition( int index );
+	//! Swap to conditions in the list.
+	void swapConditions( int i, int j );
+
+private:
+	//! Priority of the source.
+	int m_priority;
+	//! List of conditions for this source.
+	QList< Condition > m_conditions;
+}; // class Properties
+
+} /* namespace Globe */
+
+#endif // GLOBE__PROPERTIES_HPP__INCLUDED
